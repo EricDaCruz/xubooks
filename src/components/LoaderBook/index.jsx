ecf5913401bff0dcdc3ed6c8fb0b5cbde880da84
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-10-19 21:00:17 -0300

    :sparkles: feat: Fazendo load

---
+++ src/components/LoaderBook/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export function LoaderBook() {
+   return (
+      <div class="loader book">
+         <figure class="page"></figure>
+         <figure class="page"></figure>
+         <figure class="page"></figure>
+      </div>
+   );
+}
