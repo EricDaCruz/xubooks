d273a70796a4a5c49cff4ae6343d4a86fbda1bee
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-10-19 21:53:53 -0300

    :construction: Montando as informações do livro

---
+++ src/db/books.js
@@ -0,0 +1,36 @@
+export const Books = [
+   {
+      id: 1,
+      title: "Capitão America",
+      review:
+         "A primeira aparição do Capitão América foi na HQ de edição N° 1 do herói, escrita em 1941 pelo roteirista Joe Simon e ilustrada pelo desenhista Jack Kirby. O início da história é a mesma que vimos nos atuais filmes, Steve Rogers, um rapaz com problemas de saúde e não muito atlético, é escolhido para um experimento idealizado pelo governo dos EUA de criar um “Super Soldado” para combater os nazistas e seus aliados. A criação desse personagem era uma clara propaganda de guerra para o público estadunidense, onde o Capitão América era a representação dos soldados e do povo da Terra do Tio Sam, que louvasse a democracia americana, que espancasse nazistas e que fosse a voz de esperança para o imaginário popular.",
+      banner:
+         "https://2.bp.blogspot.com/-9lfZlitbFtQ/VyUCK762kWI/AAAAAAAADkA/PPF_hP-lR5M6L0EJA_zSFCDHZNFftNgrQCLcB/s1600/01.jpg",
+      genre: ["Ação", "Aventura", "Ficção Científica"],
+      author: "Joe Simon",
+      releaseYear: "1941",
+      urlPDF: "resenha-capitão-america.pdf",
+   },
+   {
+      id: 2,
+      title: "Cidade de Deus",
+      review:
+         "O filme conta a história da Cidade de Deus, um bairro de classe baixa a do Rio de Janeiro. O filme é contado do ponto de vista de um menino chamado Busca Pé que cresce lá como filho de um peixeiro e demonstra o desespero e a violência inerentes às favelas. Baseado em uma história real, o filme retrata o abuso de drogas, crimes violentos e a luta de um menino para se libertar das garras das favelas.",
+      banner: "https://m.media-amazon.com/images/I/51cY0Lsf1DL.jpg",
+      genre: ["Drama", "Crime", "Suspense"],
+      author: "Paulo Lins",
+      releaseYear: "1997",
+      urlPDF: "cidade-de-deus.pdf",
+   },
+   {
+      id: 3,
+      title: "A Hora da Estrela",
+      review:
+         "A Hora Estrela é o último livro da autora brasileira Clarice Lispector, lançado em 1977, pouco antes da autora falecer devido a um câncer no ovário. A obra pertence a Terceira Geração Modernista (1945-1980), sendo as obras dessa geração marcadas por um forte aprofundamento do psicológico dos personagens, o que não poderia faltar em A Hora da Estrela. O livro se revela como um romance intimista e autobiográfico, com Clarice tentando se “esconder” utilizando o alter ego de Rodrigo S.M, um autor fictício",
+      banner: "https://m.media-amazon.com/images/I/810Vj9zyi-L.jpg",
+      genre: ["Romance", "Romance psicológico"],
+      author: "Clarice Lispector",
+      releaseYear: "1977",
+      urlPDF: "resenha-a-hora-da-estrela.pdf",
+   },
+];
