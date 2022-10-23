3ea9b8ef046deb55c1f9d7e45d4e94fb187d342d
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-10-23 19:02:53 -0300

    :construction: Adicionando link dos livros

--- src/db/books.js
+++ src/db/books.js
@@ -10,23 +10,25 @@ export const Books = [
       author: "Joe Simon",
       releaseYear: "1941",
       urlPDF: "resenha-capitão-america.pdf",
+      urlBook: "https://hqdragon.com/leitor/Capitao_America_Comics_-_1941/01"
    },
    {
       id: 2,
       title: "Cidade de Deus",
       review:
-         "O filme conta a história da Cidade de Deus, um bairro de classe baixa a do Rio de Janeiro. O filme é contado do ponto de vista de um menino chamado Busca Pé que cresce lá como filho de um peixeiro e demonstra o desespero e a violência inerentes às favelas. Baseado em uma história real, o filme retrata o abuso de drogas, crimes violentos e a luta de um menino para se libertar das garras das favelas.",
+      "O filme conta a história da Cidade de Deus, um bairro de classe baixa a do Rio de Janeiro. O filme é contado do ponto de vista de um menino chamado Busca Pé que cresce lá como filho de um peixeiro e demonstra o desespero e a violência inerentes às favelas. Baseado em uma história real, o filme retrata o abuso de drogas, crimes violentos e a luta de um menino para se libertar das garras das favelas.",
       banner: "https://m.media-amazon.com/images/I/51cY0Lsf1DL.jpg",
       genre: ["Drama", "Crime", "Suspense"],
       author: "Paulo Lins",
       releaseYear: "1997",
       urlPDF: "cidade-de-deus.pdf",
+      urlBook: 'https://asdfiles.com/26ix5~pdfviewer'
    },
    {
       id: 3,
       title: "A Hora da Estrela",
       review:
-         "A Hora Estrela é o último livro da autora brasileira Clarice Lispector, lançado em 1977, pouco antes da autora falecer devido a um câncer no ovário. A obra pertence a Terceira Geração Modernista (1945-1980), sendo as obras dessa geração marcadas por um forte aprofundamento do psicológico dos personagens, o que não poderia faltar em A Hora da Estrela. O livro se revela como um romance intimista e autobiográfico, com Clarice tentando se “esconder” utilizando o alter ego de Rodrigo S.M, um autor fictício",
+      "A Hora Estrela é o último livro da autora brasileira Clarice Lispector, lançado em 1977, pouco antes da autora falecer devido a um câncer no ovário. A obra pertence a Terceira Geração Modernista (1945-1980), sendo as obras dessa geração marcadas por um forte aprofundamento do psicológico dos personagens, o que não poderia faltar em A Hora da Estrela. O livro se revela como um romance intimista e autobiográfico, com Clarice tentando se “esconder” utilizando o alter ego de Rodrigo S.M, um autor fictício",
       banner: "https://m.media-amazon.com/images/I/810Vj9zyi-L.jpg",
       genre: ["Romance", "Romance psicológico"],
       author: "Clarice Lispector",
@@ -43,5 +45,6 @@ export const Books = [
       author: "Racionais MC's",
       releaseYear: "1997",
       urlPDF: "resumo-sobrevivendo-no-inferno.pdf",
+      urlBook: "https://books.google.com.br/books?id=YjZuDwAAQBAJ&newbks=0&printsec=frontcover&hl=pt-BR#v=onepage&q&f=false"
    },
 ];
