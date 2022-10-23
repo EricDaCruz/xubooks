d445a931bbb7ae13b44242d7fc1e94686152ef5d
Eric da Cruz Rafael <rafaeleric40@gmail.com>
2022-10-23 19:44:29 -0300

    :sparkles: feat: Alterando texto e estilos

--- src/Pages/Community/index.jsx
+++ src/Pages/Community/index.jsx
@@ -1,9 +1,9 @@
 import { Header } from "../../components/Header";
-import { CardBook } from '../../components/CardBook';
+import { CardBook } from "../../components/CardBook";
 import { ImArrowLeft2 } from "react-icons/im";
 import { Link } from "react-router-dom";
 /* Styles */
-import './styles.css';
+import "./styles.css";
 /* Info */
 import { Books } from "../../db/books";
 
@@ -15,14 +15,12 @@ export function Community() {
             <Link to="/">
                <ImArrowLeft2 />
             </Link>
-            <h1>Comunidade</h1>
-            <div className="content">
-            {
-               Books.map((book, index) => (
-                  <CardBook key={index} book={book} community={true}/>
-               ))
-            }
+            <h1><strong>Comunidade</strong></h1>
          </div>
+         <div className="content">
+            {Books.map((book, index) => (
+               <CardBook key={index} book={book} community={true} />
+            ))}
          </div>
       </div>
    );
